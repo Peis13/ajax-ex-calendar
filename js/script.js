27cd2f3131807424b6adb625b4f338de4b65fa59
Nicolò <nico.paganelli91@gmail.com>
2020-06-30 16:19:11 +0200

    Ho ristrutturato il programma con data-attributes e aggiunto le funzionalità per il cambio del mese

--- js/script.js
+++ js/script.js
@@ -7,92 +7,146 @@
 $(document).ready(
   function() {
 
-    $.ajax(
+    // --------------- LOGICA --------------- //
+
+    // Imposto una data a cui fare riferimento
+    var dataObj = moment(
       {
-        url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
-        method:'GET',
-        success: function(festivita) {
-          var arrayFestivita = festivita.response;  // return: array di oggetti
+        day: 1,
+        month: 0,
+        year: 2018
+      }
+    );
+    stampaMese(dataObj);
+    stampaFestivita(dataObj);
 
-          // Creo due array di appoggio per date e nomi festività ajax
-          var arrayNomiFestivita = [];
-          var arrayDateFestivita = [];
+    // Click sul pulsante 'successivo'
+    //  --> leggo il valore dell'attributo che ha il titolo (meseCorrente)
+    //  --> lo trasformo in un oggetto moment così posso incrementare il mese di uno
+    //  --> stampo mese e festività solo se l'anno corrente è il 2018 altrimenti mostro un alert
+    $('#successivo').click(
+      function() {
+        var meseCorrente = $('#mese-corrente').attr('data-mese-corrente');
+        var momentObj = moment(meseCorrente);
+        momentObj.add(1, 'months'); // incremento il mese
 
-          // Ciclo la risposta ajax arrayFestivita per leggere le sue chiavi
-          // ogni chiave la inserisco nell'apposito array
-          for (var j = 0; j < arrayFestivita.length; j++) {
-
-            var nomeFestivita = arrayFestivita[j]['name'];
-            var dataFestivita = arrayFestivita[j]['date'];
-
-            arrayNomiFestivita.push(nomeFestivita);
-            arrayDateFestivita.push(dataFestivita);
-          }
-
-          // Una volta estrapolate date e festività, le accantono per il momento
-          // Imposto anno e mese a cui fare riferimento
-          // per andare a prendere i giorni totali del mese
-          var data = moment('2018-01');
-          var giorniMese = data.daysInMonth();
-
-          // Faccio un ciclo for con limite massimo i giorni totali del mese
-          //  --> per ogni giorno imposto il formato della data,
-          //  --> lo metto in un oggetto che poi andrò a stampare
-          //  --> infine incremento il giorno del mese
-          for (var i = 1; i <= giorniMese; i++) {
-            var giorno = data.format('D MMMM'); // 01 January
-
-            // Converto la data del mese
-            // nel formato della dataFestivita in cui mi ritorna la chiamata ajax
-            // per poter fare un confronto
-            var formatoDataAjax = data.format('YYYY-MM-DD');
-
-            // Vedo se la data in formato Ajax è inclusa nell'arrayDateFestivita creato in precedenza
-            // se vero:
-            //  --> ricavo l'indice dell'arrayDateFestivita con cui ho la corrispondeza
-            //  --> il nomeFestivita che andrò a stampare
-            //      diventa il nome allo stesso indice della data nell'arrayNomiFestivita
-            //  --> il colore della chiave 'classe' diventa rosso
-            // altrimenti 'nomeFestivita' e 'classe' rimarranno vuote
-            var colore;
-            if (arrayDateFestivita.includes(formatoDataAjax)) {
-
-              var indiceData = arrayDateFestivita.indexOf(formatoDataAjax);
-              nomeFestivita = ' - ' + arrayNomiFestivita[indiceData];
-              colore = 'rosso';
-
-            } else {
-              nomeFestivita = '';
-              colore = '';
-            }
-
-            // Creo un oggetto che mi serve per la stampa handlebars
-            var objGiorno = {
-              giorno: giorno,
-              nome: nomeFestivita,
-              classe: colore
-            };
-            stampaGiorno(objGiorno);
-
-            // Incremento il giorno di uno
-            data.add(1, 'days');
-          }
-        },
-        error: function() {
-          alert('Errore');
+        if (momentObj.year() == 2018) {
+          stampaMese(momentObj);
+          stampaFestivita(momentObj);
+        } else {
+          alert('Mi dispiace, puoi navigare solo nell\'anno 2018')
         }
       }
     );
 
+    // Click sul pulsante 'precedente'
+    //  --> leggo il valore dell'attributo che ha il titolo (meseCorrente)
+    //  --> lo trasformo in un oggetto moment così posso decrementare il mese di uno
+    //  --> stampo mese e festività solo se l'anno corrente è il 2018 altrimenti mostro un alert
+    $('#precedente').click(
+      function() {
+        var meseCorrente = $('#mese-corrente').attr('data-mese-corrente');
+        var momentObj = moment(meseCorrente);
+        momentObj.subtract(1, 'months'); // decremento il mese
+
+        if (momentObj.year() == 2018) {
+          stampaMese(momentObj);
+          stampaFestivita(momentObj);
+        } else {
+          alert('Mi dispiace, puoi navigare solo nell\'anno 2018')
+        }
+      }
+    );
+    
+    // --------------- FINE LOGICA --------------- //
+
     // --------------- FUNZIONI --------------- //
-    function stampaGiorno(obj) {
+
+    ////////// Stampa i giorni del mese
+    //  --> momentObj: è l'oggetto moment che passo come argomento
+    // return: niente
+    function stampaMese(momentObj) {
+
+      // resetto l'html della lista dei giorni così quando cambio il mese
+      // non viene appeso, ma sostituito
+      $('.giorni').html('');
+
+      // cambio il nome del mese corrente
+      // e l'attributo che mi servirà da riferimento per il cambio mese
+      $('#mese-corrente').text(momentObj.format('MMMM YYYY'));
+      $('#mese-corrente').attr('data-mese-corrente', momentObj.format('YYYY-MM-DD'));
+
+      // prendo i giorni totali del mese che mi servono per il ciclo for
+      var giorniMese = momentObj.daysInMonth();
+
+      // preparo il template che modifico nel ciclo for
       var source = $("#giorno-template").html();
       var template = Handlebars.compile(source);
 
-      var html = template(obj);
-      // console.log(html);
-      $('.giorni').append(html);
+      // Faccio un ciclo for con limite massimo i giorni totali del mese
+      //  --> per ogni ciclo creo un oggetto moment che rappresenta il giorno corrente,
+      //  --> con le info di questo oggetto, creo un oggetto che poi andrò a stampare con handlebars
+      //  --> infine incremento il giorno del mese
+      for (var i = 1; i <= giorniMese; i++) {
+        // var giorno = dataObj.format('D MMMM'); // TODO: verifica cosa farne
+
+        var giornoCorrente = moment(
+          {
+            day: i,
+            month: momentObj.month(),
+            year: momentObj.year()
+          }
+        );
+
+        var giornoHB = {
+          giorno: giornoCorrente.format('DD MMMM'),
+          giorno_corrente: giornoCorrente.format('YYYY-MM-DD')
+        };
+
+        var html = template(giornoHB);
+        $('.giorni').append(html);
+
+        // momentObj.add(1, 'd');
+      }
     }
+
+    ////////// Stampa le festività del mese
+    //  --> momentObj: è l'oggetto moment che passo come argomento
+    // return: niente
+    function stampaFestivita(momentObj) {
+
+      // Faccio una chiamata Ajax che ha come chiave 'data' dei valori passati come argomento
+      // che andranno a completare l'endpoint d'indirizzamento al server
+      $.ajax(
+        {
+          url: 'https://flynn.boolean.careers/exercises/api/holidays',
+          method:'GET',
+          data: {
+            year: momentObj.year(),
+            month: momentObj.month()
+          },
+          success: function(festivita) {
+            var arrayFestivita = festivita.response;  // return: array di oggetti
+
+            // Ciclo l'array che  mi ritorna dalla chiamata Ajax
+            //  --> leggo in ogni indice dell'array (oggetto) la chiave 'date'
+            //  --> vado a pescare il giorno della lista con attributo uguale alla chiave letta in precedenza
+            //  --> aggiungo classe e nome della festività al giorno della lista corrispondente
+            for (var i = 0; i < arrayFestivita.length; i++) {
+              var giornoFestivo = arrayFestivita[i].date;
+              var giornoCorrispondente = $('.giorno[data-giorno-corrente="'+ giornoFestivo +'"]');
+
+              giornoCorrispondente.addClass('rosso');
+              giornoCorrispondente.append(' - ' + arrayFestivita[i].name);
+            }
+          },
+          error: function() {
+            alert('Errore');
+          }
+        }
+      );
+    }
+
     // --------------- FINE FUNZIONI --------------- //
 
 ////////// FINE DOCUMENT READY
